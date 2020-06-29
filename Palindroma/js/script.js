40bfd70dcb8fb5089ccca8f4f9c9d09e5e843d43
Giovanni <tondigiovanni@gmail.com>
2020-06-29 16:03:06 +0200

    Aggiunta funzione con controlli

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -4,25 +4,48 @@
 */
 
 var btn = document.getElementById('btn');
+var messaggio = document.getElementById('messaggio');
 
 btn.addEventListener("click", function () {
     var parola = document.getElementById('parola').value;
-    var successo = true;
 
-    for (var i = 0; i < parola.length/2; i++) {
-        console.log(parola[i], parola[parola.length -1 - i]);
-        if (parola[i] == parola[parola.length - 1 - i]) {
-            console.log("Uguale");
-        } else {
-            console.log("Non uguale");
-            successo = false;
-        }
-    }
+    var successo = isPalindroma(parola);
 
-    if (successo) {
+    document.getElementById('jumbo').classList.add('transition');
+    messaggio.className = "show";
+
+    if (!parola) {
+        messaggio.innerHTML = "Inserisci una parola!";
+        messaggio.classList.add("error");
+    } else if (successo) {
         console.log("La parola è palindroma");
+        messaggio.innerHTML = "La parola è palindroma";
+        messaggio.classList.add("successo");
     } else {
         console.log("La parola non è palindroma");
+        messaggio.innerHTML = "La parola non è palindroma";
+        messaggio.classList.add("notSuccess");
     }
 
 });
+
+
+// function
+function isPalindroma(string) {
+    string = string.toLowerCase();
+    string = string.replace(/\s/g,'');
+    string = string.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+    var res = true;
+    for (var i = 0; i < string.length/2; i++) {
+        if (!(string[i] == string[string.length - 1 - i])) {
+            res = false;
+        }
+        console.log(string[i], string[string.length - 1 - i]);
+    }
+
+    if (res) {
+        return true;
+    }
+    
+    return false;
+}
